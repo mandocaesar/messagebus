8acdd3aeee69445da4a996fa5ce637fca7ffd8b5
Armand Caesar <armand@kata.ai>
2020-03-05 13:04:02 +0700

    Serializer done

---
+++ serializer/serializer_test.go
@@ -0,0 +1,32 @@
+package serializer
+
+import (
+	"mandocaesar/messagebus/message"
+	"testing"
+
+	"github.com/satori/uuid"
+	"gotest.tools/assert"
+)
+
+func MockFuncSerializeHeader(serializer Serializer) ([]byte, error) {
+	header := &message.MessageHeader{
+		MessageId:     uuid.NewV4().String(),
+		CorrelationId: uuid.NewV4().String(),
+		MessageFlags:  1,
+		MessageType:   1,
+		ReturnAddress: "amqp.libgen.com",
+	}
+
+	return serializer.Encode(header, "kata.MessageHeader")
+}
+
+func TestSerializerAsAvro(t *testing.T) {
+	avro := NewAvroSerializer()
+	avro.GetAllSchema("../schemas/")
+
+	result, err := MockFuncSerializeHeader(avro)
+	headerDecode, err := avro.GetHeader(result)
+
+	assert.Assert(t, err == nil)
+	assert.Assert(t, headerDecode.MessageId != "")
+}
